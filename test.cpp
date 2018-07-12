8410fbc7a564c622cf0f3869ec6a72e6e067b46d
clans <clans@DESKTOP-S1FOCQF>
2018-07-12 09:56:02 +0800

    添加项目文件。

---
+++ test.cpp
@@ -0,0 +1,12 @@
+﻿#include "byte_pattern.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    byte_pattern pat;
+
+    pat.find_pattern("    11   1 1? ?? ?   aa bb cc dd ee ff    ");
+    std::cout << pat.has_size(9) << std::endl;
+
+    return 0;
+}
